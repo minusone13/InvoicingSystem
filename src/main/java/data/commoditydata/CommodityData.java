5a20ff189104f76530343e694c8f03517bfa81f6
lhw <lhw13@software.nju.edu.cn>
2014-10-26 20:50:02 +0800

    库存管理人员的修改，主要针对Data层 by lhw
    添加了CommodityPO，CommodityData中的方法和blservice层相应的借口

--- src/main/java/data/commoditydata/CommodityData.java
+++ src/main/java/data/commoditydata/CommodityData.java
@@ -1,6 +1,32 @@
 package data.commoditydata;
 
+import java.util.*;
+import po.*;
 import dataservice.commoditydataservice.*;
-public class CommodityData implements CommodityDataService{
 
+public class CommodityData implements CommodityDataService{
+	public boolean addCommodity(CommodityPO po)
+	{
+		return true;
+	}
+	public CommodityListPO getAll()
+	{
+		return new CommodityListPO();
+	}
+	public ArrayList<CommodityPO> findCommodity(String name)
+	{
+		return new ArrayList<CommodityPO>();
+	}
+	public boolean deleteCommodity(String name, String model)
+	{
+		return true;
+	}
+	public boolean updateCommodity(CommodityPO po)
+	{
+		return true;
+	}
+	public boolean addCategory(String parent, String name)
+	{
+		return true;
+	}
 }
--- src/main/java/dataservice/commoditydataservice/CommodityDataService.java
+++ src/main/java/dataservice/commoditydataservice/CommodityDataService.java
@@ -1,5 +1,15 @@
 package dataservice.commoditydataservice;
 
-public interface CommodityDataService {
+import java.util.ArrayList;
 
+import po.CommodityListPO;
+import po.CommodityPO;
+
+public interface CommodityDataService {
+	public boolean addCommodity(CommodityPO po);
+	public CommodityListPO getAll();
+	public ArrayList<CommodityPO> findCommodity(String name);
+	public boolean deleteCommodity(String name, String model);
+	public boolean updateCommodity(CommodityPO po);
+	public boolean addCategory(String parent, String name);
 }
