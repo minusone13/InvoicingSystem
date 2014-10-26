0391ba68f7235cd583907b7cc27e326c1b2b238e
131250131 <275838287@qq.com>
2014-10-26 22:36:09 +0800

    把进货销售人员的单据加入PO包，并对PO 包和Vo包中的类进行了实现。

--- src/main/java/vo/PurBackSheetVO.java
+++ src/main/java/vo/PurBackSheetVO.java
@@ -1,5 +1,62 @@
 package vo;
 
+import java.util.ArrayList;
+
+import businesslogic.customerbl.StubCustomer;
+
 public class PurBackSheetVO {
-		
+	StubCustomer customer;
+	String id;
+	String commodity;//仓库？
+	ArrayList sheet;//是不是应该是个arraylist<>?
+	double money1;//总金额
+	String words;//备注
+	
+	public StubCustomer getcustomer(){
+		return customer;
+	}
+	
+	public void setCustomer(StubCustomer customer){
+		this.customer=customer;
+	}
+	
+	public String getid(){
+		return id;
+	}
+	
+	public void setid(String id){
+		this.id=id;
+	}
+	
+	public String getcommodity(){
+		return commodity;
+	}
+	
+	public void setcommodity(String commodity){
+		this.commodity=commodity;
+	}
+	
+	public ArrayList getsheet(){
+		return sheet;
+	}
+	
+	public void setsheet(ArrayList sheet){
+		this.sheet=sheet;
+	}
+	
+	public double getmoney1(){
+		return money1;
+	}
+	
+	public void setmoney1(double money1){
+		this.money1=money1;
+	}
+	
+	public String getwords(){
+		return words;
+	}
+
+	public void setwords(String words){
+		this.words=words;
+	}
 }
