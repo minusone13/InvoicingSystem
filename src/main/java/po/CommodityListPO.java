89bf25028419c19850a30cddf6c4be3ad2103b4e
lhw <lhw13@software.nju.edu.cn>
2014-10-26 21:34:09 +0800

    更改了CommodityListPO by lhw

--- src/main/java/po/CommodityListPO.java
+++ src/main/java/po/CommodityListPO.java
@@ -1,5 +1,15 @@
 package po;
 
+import java.util.*;
+
 public class CommodityListPO {
-	
+	ArrayList<CommodityPO> cats;
+	public CommodityListPO(ArrayList<CommodityPO> cats)
+	{
+		this.cats=cats;
+	}
+	public ArrayList<CommodityPO> getList()
+	{
+		return cats;
+	}
 }
