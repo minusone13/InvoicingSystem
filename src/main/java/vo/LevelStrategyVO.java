1b427a87b1601ba64f5ccebd03dc59fb402da1fb
131250208 <504757794@qq.com>
2014-10-29 14:40:41 +0800

    增加了各个策略VO的成员信息和方法

--- src/main/java/vo/LevelStrategyVO.java
+++ src/main/java/vo/LevelStrategyVO.java
@@ -1,5 +1,7 @@
 package vo;
 
+import java.util.ArrayList;
+
 import businesslogic.LevelStrategyStyle;
 import businesslogic.ReachStrategyStyle;
 import businesslogic.StrategyStyle;
@@ -8,4 +10,56 @@ public class LevelStrategyVO extends VO{
 
 	private StrategyStyle strategystyle=StrategyStyle.LevelStrategy;
 	private LevelStrategyStyle level_strategy_style;
+	
+	private int level;//客户等级
+	private double Limit;//消费金额下限
+	
+	private ArrayList<CommodityVO> alOfCommodityVo;//赠品信息数组
+	private double discountrate;//打折比例
+	private double couponrate;//代金券赠送比例
+	
+	private String StartTime;//开始日期
+	private int LastTime;//策略持续时间（天）
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public double getLimit() {
+		return Limit;
+	}
+	public void setLimit(double limit) {
+		Limit = limit;
+	}
+	public ArrayList<CommodityVO> getAlOfCommodityVo() {
+		return alOfCommodityVo;
+	}
+	public void setAlOfCommodityVo(ArrayList<CommodityVO> alOfCommodityVo) {
+		this.alOfCommodityVo = alOfCommodityVo;
+	}
+	public double getDiscountrate() {
+		return discountrate;
+	}
+	public void setDiscountrate(double discountrate) {
+		this.discountrate = discountrate;
+	}
+	public double getCouponrate() {
+		return couponrate;
+	}
+	public void setCouponrate(double couponrate) {
+		this.couponrate = couponrate;
+	}
+	public String getStartTime() {
+		return StartTime;
+	}
+	public void setStartTime(String startTime) {
+		StartTime = startTime;
+	}
+	public int getLastTime() {
+		return LastTime;
+	}
+	public void setLastTime(int lastTime) {
+		LastTime = lastTime;
+	}
 }
