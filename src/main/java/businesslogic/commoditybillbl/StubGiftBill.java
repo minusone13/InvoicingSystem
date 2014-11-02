e185e50fa77d7681900469947d768f1b956e2b00
lhw <lhw13@software.nju.edu.cn>
2014-11-02 12:34:22 +0800

    small change by lhw

--- src/main/java/businesslogic/commoditybillbl/StubGiftBill.java
+++ src/main/java/businesslogic/commoditybillbl/StubGiftBill.java
@@ -4,12 +4,14 @@ import po.PO;
 import vo.VO;
 import businesslogic.BillStyle;
 import businesslogic.GetVOandPO;
+import businesslogic.commoditybl.StubCommodity;
 import businesslogic.examinebl.Bill;
 
 public class StubGiftBill extends Bill implements GetVOandPO
 {//赠送单类，统一进入单据池管理和存储
 	private BillStyle style=BillStyle.GiftBill;
 	private String ID;
+	StubCommodity com;
 	public VO getVO()
 	{
 		return new VO();
