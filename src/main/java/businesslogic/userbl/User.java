547a968385458a354757f8460e3d9e3d0c796290
lhw <lhw13@software.nju.edu.cn>
2014-11-04 10:51:02 +0800

    按照体系结构文档添加了部分User的类

---
+++ src/main/java/businesslogic/userbl/User.java
@@ -0,0 +1,16 @@
+package businesslogic.userbl;
+
+import businesslogic.Role;
+
+public class User {
+	public boolean login(String account, String password)
+	{
+		return true;
+	}
+	//public void addRecord(Record rec)
+	public void signUp(String account, String password,String name,Role r)
+	{
+		
+	}
+	
+}
