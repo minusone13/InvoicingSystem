b6f4d69027fa6a922d5edb71de30717f82ce147e
131250208 <504757794@qq.com>
2014-11-13 22:02:43 +0800

    完善单据池的方法实现 增加了界面的素材图片

--- src/main/java/businesslogic/examinebl/StubBillPool.java
+++ src/main/java/businesslogic/examinebl/StubBillPool.java
@@ -33,93 +33,172 @@ public class StubBillPool {
 	}
 	/*需要向单据池中加入一张库存赠送单*/
 	public void add (StubGiftBill gb){
-		
+		alOfGiftBill.add(gb);
 	}
 	/*需要向单据池中加入一张报溢/报损单*/
 	public void add (StubSpillsLossBill spb){
-		
+		alOfSpillsLossBill.add(spb);
 	}
 	/*需要向单据池中加入一张库存报警单*/
 	public void add (StubAlertBill ab){
-		
+		alOfAlertBill.add(ab);
 	}
 	/*需要向单据池中加入一张进货单*/
 	public void add (StubPurSheet ps){
-		
+		alOfPurSheet.add(ps);
 	}
 	/*需要向单据池中加入一张进货退货单*/
 	public void add (StubPurBackSheet pbs){
-		
+		alOfPurBackSheet.add(pbs);
 	}
 	/*需要向单据池中加入一张销售单*/
 	public void add (StubSaleSheet ss){
-		
+		alOfSaleSheet.add(ss);
 	}
 	/*需要向单据池中加入一张销售退货单*/
 	public void add (StubSaleBackSheet sbs){
-		
+		alOfSaleBackSheet.add(sbs);
 	}
 	/*需要向单据池中加入一张收款单*/
 	public void add (StubReceiptBill rb){
-		
+		alOfReceiptBill.add(rb);
 	}
 	/*需要向单据池中加入一张付款单*/
 	public void add (StubPaymentBill pb){
-		
+		alOfPaymentBill.add(pb);
 	}
 	/*需要向单据池中加入一张现金费用单*/
 	public void add (StubCashPaymentBill cpb){
-		
+		alOfCashPaymentBill.add(cpb);
 	}
 	/*需要从单据池筛选指定状态的所有赠送单*/
 	public ArrayList<StubGiftBill> getGiftBill (BillState st){
-		return null;
+		ArrayList<StubGiftBill> result=new ArrayList<StubGiftBill>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubGiftBill temp:alOfGiftBill){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有报溢/报损单*/
 	public ArrayList<StubSpillsLossBill> getSpillsLossBill (BillState st){
-		return null;
+		ArrayList<StubSpillsLossBill> result=new ArrayList<StubSpillsLossBill>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubSpillsLossBill temp:alOfSpillsLossBill){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有库存报警单*/
 	public ArrayList<StubAlertBill> getAlertBill (BillState st){
-		return null;
+		ArrayList<StubAlertBill> result=new ArrayList<StubAlertBill>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubAlertBill temp:alOfAlertBill){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有进货单*/
 	public ArrayList<StubPurSheet> getPurSheet (BillState st){
-		return null;
+		ArrayList<StubPurSheet> result=new ArrayList<StubPurSheet>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubPurSheet temp:alOfPurSheet){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有进货退货单*/
 	public ArrayList<StubPurBackSheet> getPurBackSheet (BillState st){
-		return null;
+		ArrayList<StubPurBackSheet> result=new ArrayList<StubPurBackSheet>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubPurBackSheet temp:alOfPurBackSheet){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有销售单*/
 	public ArrayList<StubSaleSheet> getSaleSheet (BillState st){
-		return null;
+		ArrayList<StubSaleSheet> result=new ArrayList<StubSaleSheet>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubSaleSheet temp:alOfSaleSheet){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有销售退货单*/
 	public ArrayList<StubSaleBackSheet> getSaleBackSheet (BillState st){
-		return null;
+		ArrayList<StubSaleBackSheet> result=new ArrayList<StubSaleBackSheet>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubSaleBackSheet temp:alOfSaleBackSheet){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有收款单*/
 	public ArrayList<StubReceiptBill> getReceiptBill (BillState st){
-		return null;
+		ArrayList<StubReceiptBill> result=new ArrayList<StubReceiptBill>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubReceiptBill temp:alOfReceiptBill){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有付款单*/
 	public ArrayList<StubPaymentBill> getPaymentBill (BillState st){
-		return null;
+		ArrayList<StubPaymentBill> result=new ArrayList<StubPaymentBill>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubPaymentBill temp:alOfPaymentBill){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
+		
+		return result;
 		
 	}
 	/*需要从单据池筛选指定状态的所有现金费用单*/
 	public ArrayList<StubCashPaymentBill> getCashPaymentBill (BillState st){
-		return null;
+		ArrayList<StubCashPaymentBill> result=new ArrayList<StubCashPaymentBill>();//用于储存返回的单据
+		//遍历池中制定单据数组
+		for(StubCashPaymentBill temp:alOfCashPaymentBill){
+			if(temp.getState()==st){//如果单据状态符合筛选状态
+				result.add(temp);
+			}
+		}
 		
+		return result;
 	}
 	/*实时保存池中数组对象*/
 	public void save(BillStyle st){
