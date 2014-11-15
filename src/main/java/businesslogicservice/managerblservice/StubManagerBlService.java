dfe9f181bbd4b6f72db5399d133ba67c8c949652
131250208 <504757794@qq.com>
2014-11-15 23:54:54 +0800

    增加了总经理测试代码

--- src/main/java/businesslogicservice/managerblservice/StubManagerBlService.java
+++ src/main/java/businesslogicservice/managerblservice/StubManagerBlService.java
@@ -13,11 +13,11 @@ public interface StubManagerBlService {
 	/*需要查看已提交单据*/
 	public ArrayList<VO> getHandedBill ();
 	/*修改密码*/
-	public void modifyPassword (String newpassword);
+	public boolean modifyPassword (String newpassword);
     /*修改单据信息*/
-	public void changeImformationOfBill(VO billvo);
+	public boolean changeImformationOfBill(VO billvo);
 	/*通过数组中对应的单据*/
-	public void PassBill(ArrayList<VO> billvo);
+	public boolean PassBill(ArrayList<VO> billvo);
 	/*返回所有客户分层策略信息*/
 	public ArrayList<VO> ShowLevelStrategy ();
 	/*返回所有特价包策略信息*/
@@ -25,23 +25,23 @@ public interface StubManagerBlService {
 	/*返回所有满额促销策略信息*/
 	public ArrayList<VO> ShowReachStrategy  ();
 	/*需要删除一条客户分层策略*/
-	public void Remove (LevelStrategyVO ls);
+	public boolean Remove (LevelStrategyVO ls);
 	/*需要删除一条特价包策略*/
-	public void Remove (BarginStrategyVO bs);
+	public boolean Remove (BarginStrategyVO bs);
 	/*需要删除一条满额促销策略*/
-	public void Remove (ReachStrategyVO rs);
+	public boolean Remove (ReachStrategyVO rs);
 	/*需要制定一个赠送赠品的客户分层策略*/
-	public void addGiftLevelStrategy (LevelStrategyVO lsvo);
+	public boolean addGiftLevelStrategy (LevelStrategyVO lsvo);
 	/*需要制定一个折让客户分层策略*/
-	public void addDiscountLevelStrategy (LevelStrategyVO lsvo);
+	public boolean addDiscountLevelStrategy (LevelStrategyVO lsvo);
 	/*需要制定一个赠送代金券客户分层策略*/
-	public void addCouponLevelStrategy (LevelStrategyVO lsvo);
+	public boolean addCouponLevelStrategy (LevelStrategyVO lsvo);
 	/*需要制定一条特价包促销策略*/
-	public void addBarginStrategy (BarginStrategyVO bsvo);
+	public boolean addBarginStrategy (BarginStrategyVO bsvo);
 	/*需要制定一条赠送赠品的满额促销策略*/
-	public void addGiftReachStrategy (ReachStrategyVO rsvo);
+	public boolean addGiftReachStrategy (ReachStrategyVO rsvo);
 	/*需要制定一条赠送代金券的满额促销策略*/
-	public void addCouponReachStrategy (ReachStrategyVO rsvo);
+	public boolean addCouponReachStrategy (ReachStrategyVO rsvo);
 	/*需要查看经营历程*/
 	public ArrayList<VO> showBusinessProgress(String client,String operater,String warehouse,BillStyle billStyle, String StartTime,String LastTime);
 	/*需要查看销售明细*/
