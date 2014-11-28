da1c7201d90e388301743570881e3e60761b16ee
lhw13 <lhw13@software.nju.edu.cn>
2014-11-28 19:53:32 +0800

    增加了RM的一个值和所有注释 by lhw
    增加了一个invalid

--- src/main/java/vo/RM.java
+++ src/main/java/vo/RM.java
@@ -1,10 +1,11 @@
 package vo;
 
 public enum RM {
-		redundance,
-		insufficient,
-		notfound,
-		unknownerror,
-		done,
-		treeerror
+		redundance,//重复，一般用于添加一个项目0
+		insufficient,//不足，用于商品的销售或赠送
+		notfound,//没有找到，用于查找或删除
+		unknownerror,//未知错误，需重视
+		done,//成功
+		treeerror,//商品树错误
+		invalid//不合法，用于验证密码或者ID
 }
