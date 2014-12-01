6629ec3c9bc1175e3f3184645550807cca3b92cb
131250208 <504757794@qq.com>
2014-12-01 22:46:22 +0800

    界面ing

--- src/test/java/InvoicingSystem/SaleTest.java
+++ src/test/java/InvoicingSystem/SaleTest.java
@@ -1,24 +1,14 @@
 package InvoicingSystem;
 
-import businesslogic.customerbl.CustomerController;
 import businesslogic.salebillbl.salebillController;
 import businesslogicservice.customerblservice.StubCustomerBlService;
 import businesslogicservice.salebillblservice.StubSaleBillBlService;
 import junit.framework.TestCase;
 
 public class SaleTest extends TestCase{
-		StubCustomerBlService sbs=new CustomerController();
 		StubSaleBillBlService sbbs=new salebillController();
 		
-		public void testaddCustomer(){
-			boolean result1=sbs.addCustomer("csc");
-			assertTrue(result1);
-		}
-		
-		public void testdeleteCustomer(){
-			boolean result2=sbs.deleteCustomer("131");
-			assertTrue(result2);
-		}
+	
 		
 		public void getPurBackSheet(){
 			boolean result3=sbbs.getPurBackSheet("131");
