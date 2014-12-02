25139ba37c959cbdb109ea5808fd4dfada1dae00
131250131 <275838287@qq.com>
2014-12-02 18:54:22 +0800

    修改了SaleTest。 by 阿超（不知道这次是否可以通过）

--- src/test/java/InvoicingSystem/SaleTest.java
+++ src/test/java/InvoicingSystem/SaleTest.java
@@ -8,24 +8,22 @@ import junit.framework.TestCase;
 public class SaleTest extends TestCase{
 		StubSaleBillBlService sbbs=new salebillController();
 		
-	
-		
-		public void getPurBackSheet(){
+		public void testgetPurBackSheet(){
 			boolean result3=sbbs.getPurBackSheet("131");
 			assertTrue(result3);
 		}
 		
-		public void getPurSheet(){
+		public void testgetPurSheet(){
 			boolean result4=sbbs.getPurSheet("131");
 			assertTrue(result4);
 		}
 		
-		public void getSaleBackSheet(){
+		public void testgetSaleBackSheet(){
 			boolean result5=sbbs.getSaleBackSheet("131");
 			assertTrue(result5);
 		}
 		
-		public void getSaleSheet(){
+		public void testgetSaleSheet(){
 			boolean result5=sbbs.getSaleSheet("131");
 			assertTrue(result5);
 		}
