4640dc9cead5c8666162d01e92b5dd71ff00a82b
131250208 <504757794@qq.com>
2014-12-04 17:31:45 +0800

    给paymentVO加上了BillState的set和get方法，大家还有什么vo po 单据没有封装的马上封装好，提供get与set 界面完成了进销人员的管理单据2

--- src/main/java/userui/Frame.java
+++ src/main/java/userui/Frame.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import presentation.financialui.Financial;
 import presentation.userui.Login;
 
 public class Frame extends JFrame implements MouseListener{
@@ -54,18 +55,18 @@ public class Frame extends JFrame implements MouseListener{
 		mainJP.add(minus,1);
 		
 		
-		//登录面板
-		Login login=new Login();
-		mainJP.add(login,2);
-		login.setBounds(0, 0, 960, 600);
+//		//登录面板
+//		Login login=new Login();
+//		mainJP.add(login,2);
+//		login.setBounds(0, 0, 960, 600);
 		
 //		ManagerUI manager=new ManagerUI();
 //		mainJP.add(manager,2);
 //		manager.setBounds(0, 0, 960, 600);
 		
-//		Financial financial=new Financial();
-//		mainJP.add(financial,2);
-//		financial.setBounds(0, 0, 960, 600);
+		Financial financial=new Financial();
+		mainJP.add(financial,2);
+		financial.setBounds(0, 0, 960, 600);
 		//加上监听接口
 //		Login.addMouseListener(this);
 		//设置窗口可见
--- src/main/java/userui/TestFrame.java
+++ src/main/java/userui/TestFrame.java
@@ -6,8 +6,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
+
+
+import businesslogic.BillStyle;
 import presentation.WarningPanel;
 import presentation.financialui.JPfunctions;
+import presentation.saleui.JPmanageBills2;
 import presentation.managerui.MouseListenerGetXY;
 
 
@@ -32,7 +36,7 @@ public class TestFrame extends JFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		TestFrame testJP=new TestFrame(new WarningPanel("草泥马!!"));
+		TestFrame testJP=new TestFrame(new JPmanageBills2(BillStyle.SaleSheet));
 
 	}
 
