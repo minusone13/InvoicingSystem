da1960dd233580e2bcc67117edfe0136136cd2da
131250131 <275838287@qq.com>
2014-12-07 11:46:58 +0800

    Customer接口增加了两个方法。 by阿超

--- src/main/java/businesslogicservice/customerblservice/CustomerBlService.java
+++ src/main/java/businesslogicservice/customerblservice/CustomerBlService.java
@@ -1,5 +1,7 @@
 package businesslogicservice.customerblservice;
 
+import java.util.ArrayList;
+
 import vo.CustomerVO;
 import vo.RM;
 
@@ -13,6 +15,8 @@ public interface CustomerBlService {
 		public RM updateCustomer(CustomerVO vo);
 		public boolean deleteCustomer(String id);
 		public CustomerVO findCustomer(String id);
+		public ArrayList<CustomerVO> getCustomer(String address);
+		public void saveCustomer(String address);
 		
 		/*
 		 * 关于模糊查找我重新写个方法；
