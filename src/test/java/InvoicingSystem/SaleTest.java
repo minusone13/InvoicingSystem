11a8fe623067d80c85f2b0210445f5018d9fb526
131250131 <275838287@qq.com>
2014-12-12 12:44:48 +0800

    终于连上网络了。。

--- src/test/java/InvoicingSystem/SaleTest.java
+++ src/test/java/InvoicingSystem/SaleTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import vo.CustomerVO;
+import vo.SaleBackSheetVO;
 import vo.SaleSheetVO;
 import businesslogic.commoditybl.MockCommodity;
 import businesslogic.customerbl.Customer;
@@ -13,14 +14,14 @@ import businesslogicservice.salebillblservice.SaleBillBlService;
 import junit.framework.TestCase;
 
 public class SaleTest extends TestCase{
-		SaleBillBlService sbbs=new salebillController();
+		//SaleBillBlService sbbs=new salebillController();
 		public void testTrue(){//added by lhw
 			assertTrue(true);
 		}
 		
 		public void testCreatSaleSheet() {
 			SaleSheetVO vo = new SaleSheetVO();
-			Customer customer = new Customer();
+			Customer customer = new Customer(); 
 			customer.setname("梅少");
 			vo.setCustomer(customer);
 			vo.setuserid("002");
@@ -37,10 +38,30 @@ public class SaleTest extends TestCase{
 			vo.setwords("我是备注");
 			salebillController controller = new salebillController();
 			boolean result1 = controller.createSaleSheet(vo);
-			
 			assertTrue(result1);
 		}
 		
+		/*public void testCreatSaleBackSheet() {
+			SaleBackSheetVO vo = new SaleBackSheetVO();
+			Customer customer = new Customer();
+			customer.setname("梅少");
+			vo.setCustomer(customer);
+			vo.setuserid("002");
+			vo.setdate(new Date());
+			vo.setdiscount(20.0);
+			vo.setmoney1(100.0);
+			vo.setmoney2(10.0);
+			vo.setpmoney(70.0);
+			vo.setstock("1");
+			vo.setusername("学长");
+			vo.setuserid("208");
+			vo.setop("学长208");
+			vo.setsheet(new ArrayList<MockCommodity>());
+			vo.setwords("我是备注");
+			salebillController controller = new salebillController();
+			boolean result1 = controller.createSaleBackSheet(vo);
+			assertTrue(result1);
+		}*/
 		
 		/*
 		public void testgetPurBackSheet(){
