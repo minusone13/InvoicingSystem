d6b0f1852f37577081c16b63012958ffc4463ce7
131250131 <275838287@qq.com>
2014-12-31 21:23:27 +0800

    ~

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -29,6 +29,7 @@ public class Server
 	public static void main(String args[]){
 		 try
 		 {
+			 
 			 	String ip = "127.0.0.1:1099";
 				CustomerDataService customerdataservice= new CustomerData();
 				AccountDataService accountdataservice = new AccountData();
